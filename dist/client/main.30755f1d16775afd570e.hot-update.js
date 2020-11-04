6861852bc8b2f6e5952c6b478827c30ea0b1b828
andreitru <andrei.trusenko@gmail.com>
2020-11-04 18:04:13 +0300

    hmr fixed

---
+++ dist/client/main.30755f1d16775afd570e.hot-update.js
@@ -0,0 +1,104 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/@hot-loader/react-dom/cjs/react-dom.development.js":
+false,
+
+/***/ "./node_modules/@hot-loader/react-dom/index.js":
+false,
+
+/***/ "./node_modules/@hot-loader/react-dom/node_modules/scheduler/cjs/scheduler-tracing.development.js":
+false,
+
+/***/ "./node_modules/@hot-loader/react-dom/node_modules/scheduler/cjs/scheduler.development.js":
+false,
+
+/***/ "./node_modules/@hot-loader/react-dom/node_modules/scheduler/index.js":
+false,
+
+/***/ "./node_modules/@hot-loader/react-dom/node_modules/scheduler/tracing.js":
+false,
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/less-loader/dist/cjs.js!./src/shared/header.less":
+false,
+
+/***/ "./node_modules/css-loader/dist/runtime/api.js":
+false,
+
+/***/ "./node_modules/fast-levenshtein/levenshtein.js":
+false,
+
+/***/ "./node_modules/hoist-non-react-statics/dist/hoist-non-react-statics.cjs.js":
+false,
+
+/***/ "./node_modules/object-assign/index.js":
+false,
+
+/***/ "./node_modules/prop-types/checkPropTypes.js":
+false,
+
+/***/ "./node_modules/prop-types/factoryWithTypeCheckers.js":
+false,
+
+/***/ "./node_modules/prop-types/index.js":
+false,
+
+/***/ "./node_modules/prop-types/lib/ReactPropTypesSecret.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/dist/react-hot-loader.development.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/dist/react-hot-loader.production.min.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/index.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/root.js":
+false,
+
+/***/ "./node_modules/react-is/cjs/react-is.development.js":
+false,
+
+/***/ "./node_modules/react-is/index.js":
+false,
+
+/***/ "./node_modules/react-lifecycles-compat/react-lifecycles-compat.es.js":
+false,
+
+/***/ "./node_modules/react/cjs/react.development.js":
+false,
+
+/***/ "./node_modules/react/index.js":
+false,
+
+/***/ "./node_modules/shallowequal/index.js":
+false,
+
+/***/ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/amd-define.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/amd-options.js":
+false,
+
+/***/ "./src/client/index.jsx":
+/*!******************************!*\
+  !*** ./src/client/index.jsx ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/ts-loader/index.js):\\nError: ENOENT: no such file or directory, open '/home/andrei/Документы/skillbox.react/react_project/src/client/index.jsx'\");\n\n//# sourceURL=webpack:///./src/client/index.jsx?");
+
+/***/ }),
+
+/***/ "./src/shared/Header.jsx":
+false,
+
+/***/ "./src/shared/header.less":
+false
+
+})
