9c3c2d9bd2f3aae2b0916a80b1f82a6ed1289e00
andrei <andrei.trusenko@gmail.com>
2021-05-22 18:23:58 +0300

    feat: update client_id

--- src/server/server.js
+++ src/server/server.js
@@ -13,9 +13,9 @@ app.use('/static', express.static('./dist/client'));
 app.get('/auth', (req, res) => {
   axios.post(
     'https://www.reddit.com/api/v1/access_token',
-    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=https://react-skillbox.herokuapp.com/auth`,
     {
-      auth: {username: process.env.CLIENT_ID, password: 'ndPh5uNumEC5iLdv2QTQ7HUDtQ4gYQ'},
+      auth: {username: process.env.CLIENT_ID, password: process.env.SECRET},
       headers: {'Content-type': 'application/x-www-form-urlencoded'}
     }
   )
